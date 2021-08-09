08f491ac28de47ce0bc1a118c64ca6b681ced0da
Andreson Tianth <andresontianth@gmail.com>
2021-08-08 22:04:28 -0300

    Alteração de tabelas cadastros e metas

--- database/migrations/2021_08_07_022125_create_tables.php
+++ database/migrations/2021_08_07_022125_create_tables.php
@@ -14,24 +14,24 @@ class CreateTables extends Migration
     public function up()
     {
         Schema::create('cadastros', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->integer('id_indicador');
-            $table->integer('indicados');
+            $table->integer('indicados')->default(0);
             $table->string('nome_cliente', 255);
-            $table->string('nome_negocio', 255);
-            $table->string('segmento');
+            $table->string('nome_negocio', 255)->default(0);
+            $table->integer('segmento')->default(0);
             $table->integer('tipo_cliente');
             $table->string('email', 255);
-            $table->string('estado', 150);
-            $table->string('telefone', 55);
+            $table->string('estado', 150)->default('');
+            $table->string('telefone', 55)->default('');
             $table->timestamps();
         });
 
         Schema::create('metas', function (Blueprint $table) {
-            $table->id();
-            $table->integer('qt_indicados');
+            $table->increments('id');
+            $table->integer('qt_indicados')->default(0);
             $table->string('tipo_bonus', 150);
-            $table->integer('valor_bonus');
+            $table->integer('valor_bonus')->default(0);
             $table->timestamps();
         });
     }
